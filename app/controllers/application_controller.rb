73e3ee6c66bca6a85ce3226d018c557d274ac579
Vlad Snisar <snvl1993@gmail.com>
2016-09-28 23:58:51 +0300

    Rescue from record not found and invalid record exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { errors: e.to_s }, status: 400
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { errors: e.to_s }, status: 400
+  end
 end
